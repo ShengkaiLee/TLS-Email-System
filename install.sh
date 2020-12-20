a73f1c3efcdc32949ab9bf6b667f44a85b23547a
Shengkai <sl4685@columbia.edu>
2020-12-20 15:49:55 -0500

    add 00004

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ cd ..
 mv 00001 project/input
 mv 00002 project/input
 mv 00003 project/input
+mv 00004 project/input
 mv server project/bin
 mv sendmsg project/bin
 mv recvmsg project/bin
 mv signer.pem project/ca/intermediate/certs
 mv cacert.pem project/ca/intermediate/certs
+bash sandbox.sh
